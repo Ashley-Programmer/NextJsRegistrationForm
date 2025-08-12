b14f50383a7be14eacad2d8feb3da2cca13259f1
Ashley K Motsie <motsieashley31@gmail.com>
2025-08-12 21:51:39 +0200

    Initial commit

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,11 +1,26 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function Registration() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response = await axios.post('/api/register', {name, email, password});
+        console.log(response);
+    }
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 dark:bg-gray-100 transition-colors duration-300">
       <div className="w-full max-w-md p-6 bg-white dark:bg-white rounded-lg shadow-lg">
         <h1 className="text-3xl font-bold mb-8 text-center text-gray-900 dark:text-black">
           Register Now
         </h1>
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
             <label
               htmlFor="name"
@@ -18,6 +33,7 @@ export default function Registration() {
               id="name"
               className="w-full px-4 py-2 border border-gray-400 dark:border-gray-600 rounded-md bg-white dark:bg-gray-100 text-gray-900 dark:text-gray-900 placeholder-gray-400 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400 transition-colors duration-200"
               placeholder="Enter your Names"
+              onChange={(e) => setName(e.target.value)}
               required
             />
           </div>
@@ -33,6 +49,7 @@ export default function Registration() {
               id="email"
               className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-100 text-gray-900 dark:text-gray-900 placeholder-gray-400 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400 transition-colors duration-200"
               placeholder="Enter your email"
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
@@ -48,6 +65,7 @@ export default function Registration() {
               id="password"
               className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-100 text-gray-900 dark:text-gray-900 placeholder-gray-400 dark:placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400 transition-colors duration-200"
               placeholder="Create a password"
+              onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
